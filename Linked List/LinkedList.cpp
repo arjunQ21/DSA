2025a00221dfb1e1a8bcb2397a44a290fdc79532
Arjun Adhikari <46340446+arjunQ21@users.noreply.github.com>
2020-02-13 09:40:41 +0545

    Update LinkedList.cpp

--- Linked List/LinkedList.cpp
+++ Linked List/LinkedList.cpp
@@ -65,12 +65,34 @@ class LinkedList{
 				cout << "\nNo items in linked list.";
 			}
 		}
+		void insertAfterANode(int data, int dataInNodeToInsertAfter){
+			struct Node * temp = start ;
+			int inserted = 0 ;
+			while(temp->next != NULL){
+				if(temp->data == dataInNodeToInsertAfter){
+					struct Node * newNode = createNode() ;
+					if(!newNode){
+						cout << "\nUnable to create new node.";
+						return ;
+					}
+					newNode->data = data ;
+					newNode->next = temp->next ;
+					temp->next = newNode ;
+					inserted = 1 ;
+				}
+				if(inserted) break ;
+				temp = temp->next ;
+			}
+			if(!inserted) cout << "\nData "<< dataInNodeToInsertAfter << " not found in linked list." ;
+			if(inserted) cout << "\nInserted a node with data "<< data <<" after a node with data " << dataInNodeToInsertAfter ;
+		}
 };
 
 main(){
 	LinkedList l ;
 	l.insertAtLast(5);
 	l.insertAtLast(8);
+	l.insertAfterANode(7, 5);
 	l.insertAtFirst(9) ;
 	l.display();
 }
