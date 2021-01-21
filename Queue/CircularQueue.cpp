913df27ccb5b5648bb22a0a9dabd3d4b0da6cca1
arjunQ21 <arjunadhikari789@gmail.com>
2021-01-21 21:56:02 +0545

    Circular Queue

--- Queue/CircularQueue.cpp
+++ Queue/CircularQueue.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
+#include <conio.h>
 
 #define SIZE 5
 
 using namespace std ;
 
-class Queue{
+class CircularQueue{
 	//Usually, rear and front are used. insertAt and deleteFrom are more readable. so,
 	int insertAt , deleteFrom , arr[SIZE] ;
 	public:
-		Queue(){
+		CircularQueue(){
 			deleteFrom = 0 ;
 			insertAt = 0 ;
 			cout << "\nQueue initialized." ;
 			show() ;
 		}
 		int isFull(){
-			return (insertAt == SIZE) ;
+			return ((insertAt - deleteFrom) ==  SIZE) ;
 		}
 		int isEmpty(){
 			return (insertAt == deleteFrom) ;
 		}
 		void insert(int a){				
 			if(!isFull()){
-				arr[ insertAt++ ] = a ;
+				arr[ (insertAt++ % SIZE) ] = a ;
 				cout << "\nInserted " << a << " to queue." ;
 			}else{
 				cout << "\nQueue full. cant insert " << a ;
@@ -32,22 +33,18 @@ class Queue{
 		
 		void remove(){
 			if(!isEmpty()){
-				cout << "\nRemoved " << arr[deleteFrom++] << " from queue.";
+				cout << "\nRemoved " << arr[(deleteFrom++ % SIZE)] << " from queue.";
 			}else{
 				cout << "\nCant remove, no items in queue." ;
-				reset() ;
 			}
 			show() ;
 		}
-		void reset(){
-			deleteFrom = insertAt = 0 ;
-			cout << "\nQueue Reset." ;
-		}
 		void show(){
 			cout << "\nCurrent Queue Status: " ;
+			cout << "\n\tdeleteFrom: " << deleteFrom << ", insertAt: " << insertAt  ;
 			if(!isEmpty()){
 				for(int i = deleteFrom; i < insertAt; i++){
-					cout << "\n\t[" << i << "] = " << arr[i] ;
+					cout << "\n\t[" << i%SIZE << "] = " << arr[i % SIZE] ;
 				}
 				cout << "\n" ;
 			}else{
@@ -57,7 +54,8 @@ class Queue{
 };
 
 main(){
-	Queue queue ;
+	CircularQueue queue ;
+	//for exams, you can just show these
 	queue.insert(1);
 	queue.insert(23);
 	queue.insert(3);
@@ -73,4 +71,31 @@ main(){
 	queue.remove();
 	queue.remove();
 	queue.insert(6);	
+	
+	//For Testing nicely in console screen
+	char choice = 1 ;
+	do{
+		cout << "\n\nCicular Queue Demo. Press given numbers for respective functions: ";
+		cout << "\n1\tInsert item into queue."
+			<< "\n2\tRemove item from queue."
+			<< "\n3\tShow Queue Status."
+			<< "\n0\tExit."<< endl;
+		choice = getche() ;
+		switch(choice){
+			case '1':
+				cout << "\nEnter a number to add to queue: " ;
+				int a ;
+				cin >> a ;
+				queue.insert(a) ;
+				break ;
+			case '2':
+				queue.remove() ;
+				break ;
+			case '3':
+				queue.show() ;
+				break ;
+			default:
+				cout << "\nInvalid Choice. choose again." ;
+		}
+	}while(choice != '0');
 }
